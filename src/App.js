0b58102a78c7f2c974c240c89b7cac183d75013f
James Cook <jscook@Jamess-MacBook-Pro.local>
2023-02-07 19:16:46 -0500

    changes for v2

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,17 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          And we're live!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Take me to a webpage
-        </a>
+        <h1>Hello from V2</h1>
       </header>
     </div>
   );
